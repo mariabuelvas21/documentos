f6215a79946127d69cf2093f9aee824fc2226802
mariabuelvas21 <112511200+mariabuelvas21@users.noreply.github.com>
2022-09-02 13:16:47 -0500

    Add files via upload

---
+++ c# algoritmo personalisado.cs
@@ -0,0 +1,30 @@
+﻿int edad;
+Console.WriteLine ("cual es su nombre");
+          string nombre = Console.ReadLine ();
+
+     Console.WriteLine ("cual es su edad");
+           edad = Convert.ToInt32 ( Console.ReadLine ()); 
+
+   if (edad >=18 ) {
+      Console.WriteLine ("Es mayor de edad "+nombre);
+   } else {
+       Console.WriteLine ("Es menor de edad "+nombre);
+   } 
+
+   do
+   {
+      Console.WriteLine("cual es su nombre");
+                 nombre = Console.ReadLine ();
+
+      Console.WriteLine("cual es su edad");
+                 edad = Convert.ToInt32 ( Console.ReadLine ());
+
+   }
+   while(edad<=18); 
+      Console.WriteLine ("no entra al aula"); 
+
+
+
+
+
+   
